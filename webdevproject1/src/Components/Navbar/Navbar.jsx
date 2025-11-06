d8202e61ee7f6c10c303431ec51fe83cbaa6bf91
Reena kumari <reenakvs1@gmail.com>
2025-11-06 16:17:59 +0530

    changes

--- webdevproject1/src/Components/Navbar/Navbar.jsx
+++ webdevproject1/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./Navbar.css"
-import logoWhite from "../../assets/logo-white.png"
+import logoLlight from "../../assets/logo-black.png"
+import logoDark from "../../assets/logo-white.png"
+
 import searchWhite from "../../assets/serach-white-thin.png"
 import searchBlack from "../../assets/search-black-thin.png"
+
+import toggleLight from '../../assets//black-sun.png'
+import logo_light from "../../assets/logo-white.png"
+
 import logo from "../../assets/correct-logo.png"
 import darkTheme from "../../assets/black-sun.png"
 import lightTheme from "../../assets/sun-white.png"
